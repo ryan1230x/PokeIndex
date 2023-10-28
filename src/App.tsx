dc5e8cb3287fdfba3f538b72009cb7bc5481c684
Ryan Harper <ryan1230tenerife@gmail.com>
2023-10-28 14:59:31 +0100

    project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios";
+import { ReactNode, useEffect, useState } from "react";
+
+const POKEMON_URL = "https://pokeapi.co/api/v2/pokemon";
+
+interface IPokemonListProps {
+  children: ReactNode;
+}
+
+interface IPokemonResult {
+  name: string;
+  url: string;
+}
+
+interface IPokemonCard {
+    pokemon: IPokemonResult
+}
+
+const Header = () => {
+  return (
+    <header>
+      <p>pokeindex</p>
+      <form>
+        <input type="search" name="" id="" placeholder="Search" />
+      </form>
+    </header>
+  );
+};
+
+const Footer = () => {
+  return <footer>Pokeindex 2024</footer>;
+};
+
+const NavigationLinks = () => {
+  return (
+    <>
+      <h1>Links</h1>
+      <ul>
+        <li>About</li>
+        <li>Version</li>
+      </ul>
+    </>
+  );
+};
+
+const PokemonList = ({ children }: IPokemonListProps) => {
+  return <section className="grid-container">{children}</section>;
+};
+
+const PokemonCard = ({ pokemon }: IPokemonCard) => {
+  return (
+    <div className="pokemon-card">
+      <div>
+        <p>{pokemon.name}</p>
+      </div>
+      <button>view details</button>
+    </div>
+  );
+};
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemon, setPokemon] = useState<IPokemonResult[] | undefined>([]);
+
+  useEffect(() => {
+    axios
+      .get(`${POKEMON_URL}/?limit=20`)
+      .then((data) => setPokemon(data.data.results));
+  }, [setPokemon]);
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <NavigationLinks />
+      <PokemonList>
+        {pokemon && pokemon.map((pokemon, index) => (
+          <PokemonCard key={index} pokemon={pokemon} />
+        ))}
+      </PokemonList>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
